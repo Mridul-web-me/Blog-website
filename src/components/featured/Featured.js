af75d65ef8a8bad0e7f35e0f7436753604fca569
Mridul Islam <md.mridul.23@gmail.com>
2023-12-31 14:22:32 +0600

    featured added

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import styles from './featured.module.css';
+import Image from 'next/image';
 
 const Featured = () => {
-  return <div>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        {' '}
+        <b>Hey, BTech here!</b> Discover our stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem Ipsum Lorem Ipsum Lorem Ipsum</h1>
+          <p className={styles.postDesc}>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
